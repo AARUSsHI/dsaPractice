5a986a64b70f80e04c667a939fdc2408e740888d
Aarushi <aarushitarun18@gmail.com>
2024-10-25 19:47:00 +0530

    DSA

--- Day02/DoublyLinkedList/DoublyLinkedList.java
+++ Day02/DoublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,57 @@
 package Day02.DoublyLinkedList;
 
 public class DoublyLinkedList {
-    
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+    public static class ListNode{
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+    public int length(){
+        return length;
+    }
+
+    public void displayForward() {
+        if (head == null) {
+            return;
+        }
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward() {
+        if (tail == null) {
+            return;
+        }
+        ListNode temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+
+
+    public static void main(String[] args) {
+        
+    }
 }
