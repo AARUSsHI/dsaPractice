06f0616380c2f6dc1d7c5e07d73e24fcfbfe1f3e
Aarushi <aarushitarun18@gmail.com>
2025-01-05 22:13:12 +0530

    Java

--- Day07/Array/main.java
+++ Day07/Array/main.java
@@ -2,7 +2,7 @@ package Day07.Array;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void printArr(int[] arr){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
@@ -82,32 +82,96 @@ public class main {
         return max;
     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        main obj = new main();
-        int len = input.nextInt();
-        int[] arr = new int[len];
+    public static int [] moveZeroes(int [] arr){
+        int j = 0;
         for(int i=0; i<arr.length; i++){
-            arr[i] = input.nextInt();
+            if(arr[i] !=0 && arr[j] != 0){
+                j++;
+            }
+            if(arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+        return arr;
+    }
 
-        printArr(arr);
+    public int [] resize(int [] arr, int n){
+        int arr1[] = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr1[i] = arr[i];
+        }
+        return arr1;
+    }
 
-        int[] result = obj.removeEven(arr);
-        printArr(result);
+    public int findMissingValue(int[] arr){
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+        for(int i=0; i<arr.length; i++){
+            sum -= arr[i];
+        }
+        return sum;
+    }
 
-        reverseArr(arr, 0, arr.length - 1);
-        printArr(arr);
+    public boolean isPalendrome(String words){
+        char [] arr = words.toCharArray();
+        int start = 0;
+        int end = words.length() -1;
+        while(start < end){
+            if(arr[start] != arr[end]){
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
 
-        int minimum = obj.min(arr);
-        System.out.println("minimum value" + minimum);
-
-        int secondMax = obj.secondMax(arr);
-        System.out.println("second maximum value" + secondMax);
-
-        int maximum = obj.max(arr);
-        System.out.println("maximum value" + maximum);
-
+    public static void main(String[] args) {
+        Main obj = new Main();
+        Scanner input = new Scanner(System.in);
+        String words = input.next();
+        System.out.println(obj.isPalendrome(words));
         input.close();
+        // int [] arr = {2,4,1,8,6,3,7};
+
+        // int missingValue = obj.findMissingValue(arr);
+        // System.out.println(missingValue);
+
+        // moveZeroes(arr);
+        // printArr(arr);
+
+        // int []arr1 = obj.resize(arr, 10);
+        // printArr(arr1);
+
+        // Scanner input = new Scanner(System.in);
+        // Main obj = new Main();
+        // int len = input.nextInt();
+        // int[] arr = new int[len];
+        // for(int i=0; i<arr.length; i++){
+        //     arr[i] = input.nextInt();
+        // }
+
+        // printArr(arr);
+
+        // int[] result = obj.removeEven(arr);
+        // printArr(result);
+
+        // reverseArr(arr, 0, arr.length - 1);
+        // printArr(arr);
+
+        // int minimum = obj.min(arr);
+        // System.out.println("minimum value " + minimum);
+
+        // int secondMax = obj.secondMax(arr);
+        // System.out.println("second maximum value " + secondMax);
+
+        // int maximum = obj.max(arr);
+        // System.out.println("maximum value " + maximum);
+
+        // input.close();
     }
 }
